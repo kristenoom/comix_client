e1d34656d3790d3c8fbe8472002c2626354b0b34
kristenoom <kris.a.oom@gmail.com>
2021-09-07 20:50:20 -0400

    created table for user input on comix and wishlist components

--- src/components/Comix.jsx
+++ src/components/Comix.jsx
@@ -5,6 +5,26 @@ const Comix = () => {
         <div className="main">
             <div className="mainDiv">
                 <h1 className="heading">Comic Book Log</h1>
+                <p className="text">Add a comic book to your reading list here.</p>
+                {/* insert form here */}
+                <br />
+                <table className="text" width="600px" maxWidth="1000px">
+                    <thead>
+                        <tr>
+                            {/* <td width="10%" align="center">Cover Art</td> */}
+                            <td width="60%"><strong>Title</strong></td>
+                            <td width="30%" align="center"><strong>Issue Date</strong></td>
+                            <td width="10%" align="center"><strong>Have Read</strong></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {/* <td width="10%" align="center">IMAGE HERE</td> */}<td width="60%">Lorem Ipsum</td>
+                            <td width="30%" align="center">Date 2021</td>
+                            <td width="10%" align="center">✔️</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -5,6 +5,25 @@ const Wishlist = () => {
         <div className="main">
             <div className="mainDiv">
                 <h1 className="heading">Wishlist</h1>
+                <p className="text">Add a comic book to your wishlist here.</p>
+                {/* insert form here */}
+                <br />
+                <table className="text" width="600px" maxWidth="1000px">
+                    <thead>
+                        <tr>
+                            <td width="60%"><strong>Title</strong></td>
+                            <td width="30%" align="center"><strong>Issue Date</strong></td>
+                            <td width="10%" align="center"><strong>Want</strong></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td width="60%">Lorem Ipsum</td>
+                            <td width="30%" align="center">Date 2021</td>
+                            <td width="10%" align="center">✔️</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
