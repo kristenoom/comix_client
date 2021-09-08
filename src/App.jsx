de21c545743e34af7c60b855cc7378574a8abd28
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-09-07 20:57:42 -0400

    Login/Signup update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css';
+import React, {useEffect, useState} from 'react'
+import Sitebar from './components/site/Header'
+import Auth from './auth/Auth'
+
 
 function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'))
+    }
+  }, []);
+
+  const updateToken = (newToken)=> {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken)
+    console.log(sessionToken);
+  }
 
 
   return (
     <div className="App">
-    
+    <Sitebar/>
+    <Auth/>
     </div>
   );
 }
