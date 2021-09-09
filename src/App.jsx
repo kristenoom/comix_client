cb62c5e0f170d90e6ecfed3af6382e09d7df7951
Kristen O <kris.a.oom@gmail.com>
2021-09-09 19:01:25 -0400

    update Home.jsx and Sidebar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import React, {useEffect, useState} from 'react'
-import Sitebar from './components/site/Header'
-import Auth from './auth/Auth'
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+//import Sitebar from './components/site/Header';
 
 
-function App() {
-  const [sessionToken, setSessionToken] = useState('');
+import { BrowserRouter as Router } from 'react-router-dom';
 
-  useEffect(() => {
-    if (localStorage.getItem('token')){
-      setSessionToken(localStorage.getItem('token'))
+
+const App = () => {
+    const [sessionToken, setSessionToken] = useState('');
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            setSessionToken(localStorage.getItem('token'))
+        }
+    }, []);
+
+    const updateToken = (newToken) => {
+        localStorage.setItem('token', newToken);
+        setSessionToken(newToken)
+        console.log(sessionToken);
     }
-  }, []);
-
-  const updateToken = (newToken)=> {
-    localStorage.setItem('token', newToken);
-    setSessionToken(newToken)
-    console.log(sessionToken);
-  }
 
 
-  return (
-    <div className="App">
-    <Sitebar/>
-    <Auth/>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <Header />
+            <Router>
+                <Sidebar />
+            </Router>
+            {/* <Sitebar /> */}
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
