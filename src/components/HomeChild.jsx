55fcb3d1a222d38ed7e57acb5b0f47305d0aaeb1
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-09-25 08:02:32 -0400

    comic card stytle

--- src/components/HomeChild.jsx
+++ src/components/HomeChild.jsx
@@ -11,17 +11,21 @@ const HomeChild = (props) => {
             {props.results.map(result => {
                 return (
                     <div key={result.id}>
-                    <Card>
-                        <CardImg top width="50%" src={result.image.small_url} alt={result.name} />
-                        <CardBody>
-                            <CardTitle tag="h5">{result.name}</CardTitle>
-                            <CardSubtitle tag="h6" className="mb-2 text-muted">{result.volume.name}</CardSubtitle>
-                            <CardText>
-                                Issue Number: {result.issue_number}<br />
-                                Cover Date: {result.cover_date}<br />
-                                Description: {result.description}
-                            </CardText>
-                        </CardBody>
+                    <Card style={{width: "300px"}}>
+                            <CardBody>
+                                <CardTitle tag="h5" className="text"><strong className="text-muted">Title:</strong> {result.volume.name}</CardTitle>
+                                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                                    {/* <strong>Volume:</strong> {result.volume} */}
+                                </CardSubtitle>
+                            </CardBody>
+                            <CardImg top width="100%" src={result.image.small_url} alt={result.volume.name} />
+                            <CardBody>
+                                <CardText className="text">
+                                    <strong>Issue Number:</strong> {result.issue_number}<br />
+                                    <strong>Cover Date:</strong> {result.cover_date}<br />
+                                    <strong>Description:</strong> {result.description}                                    
+                                </CardText>
+                            </CardBody>
                     </Card>
                     </div>
                 )
