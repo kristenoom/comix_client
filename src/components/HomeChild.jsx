82b72211c0edad2d010433704d823f44e52c3d47
Kristen Oom <kris.a.oom@gmail.com>
2021-09-25 07:16:25 -0400

    update HomeChild stylizing

--- src/components/HomeChild.jsx
+++ src/components/HomeChild.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
+import {CardColumns, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 
 const HomeChild = (props) => {
     //const { name, description, image, id, cover_date, issue_number } = props.comic;
@@ -10,20 +10,24 @@ const HomeChild = (props) => {
             {props.results.map(result => {
                 return (
                     <div key={result.id}>
-                    <Card style={{width: '100px'}}>
-                        <CardImg top src={result.image.small_url} alt={result.volume.name} />
-                        <CardBody>
-                            <CardTitle tag="h5" className="text"><strong className="text-muted">Title:</strong> {result.volume.name}</CardTitle>
+                    <CardColumns>
+                        <Card style={{ width: '200px' }}>
+                            <CardBody>
+                                <CardTitle tag="h5" className="text"><strong className="text-muted">Title:</strong> {result.volume.name}</CardTitle>
                                 <CardSubtitle tag="h6" className="mb-2 text-muted">
                                     {/* <strong>Volume:</strong> {result.volume} */}
                                 </CardSubtitle>
-                            <CardText className="text">
-                                <strong>Issue Number:</strong> {result.issue_number}<br />
-                                <strong>Cover Date:</strong> {result.cover_date}<br />
-                                <strong>Description:</strong> {result.description}                                    
-                            </CardText>
-                        </CardBody>
+                            </CardBody>
+                            <CardImg top src={result.image.small_url} alt={result.volume.name} />
+                            <CardBody>
+                                <CardText className="text">
+                                    <strong>Issue Number:</strong> {result.issue_number}<br />
+                                    <strong>Cover Date:</strong> {result.cover_date}<br />
+                                    <strong>Description:</strong> {result.description}                                    
+                                </CardText>
+                            </CardBody>
                     </Card>
+                    </CardColumns>
                     </div>
                 )
             })}
